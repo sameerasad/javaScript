b107eb43537f15087bbcd7a482067464e969cb3a
Sameer Asad <51676937+sameerasad@users.noreply.github.com>
2022-12-13 08:35:45 -0800

    demo

---
+++ Basic_Part_01/42.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const asd = () => {
+  return (
+    <View>
+      <Text>42</Text>
+    </View>
+  );
+};
+
+export default asd;
